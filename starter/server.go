ed86e8d6087dc29bdaaeb990719b7e1355ee4255
Aleksey Kish <kish94@mail.ru>
2024-09-18 00:49:56 +0200

    grpc wrapper for starter

--- starter/server.go
+++ starter/server.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net"
 	"net/http"
+
+	"google.golang.org/grpc"
 )
 
 type Server interface {
@@ -13,6 +16,7 @@ type Server interface {
 }
 
 var errServerIsNil = errors.New("server is nil")
+var errListenerIsNil = errors.New("listener is nil")
 
 type HTTPWrapper struct {
 	server *http.Server
@@ -42,6 +46,37 @@ func (s *HTTPWrapper) Shutdown() error {
 	if err := s.server.Shutdown(context.Background()); err != nil {
 		return fmt.Errorf("http wrapper shutdown: %w", err)
 	}
+
+	return nil
+}
+
+type GRPCWrapper struct {
+	server   *grpc.Server
+	listener net.Listener
+}
+
+func WrapGRPC(server *grpc.Server, listener net.Listener) Server {
+	return &GRPCWrapper{server: server, listener: listener}
+}
+
+func (s *GRPCWrapper) Serve() error {
+	if s.server == nil {
+		return errServerIsNil
+	}
+
+	if s.listener == nil {
+		return errListenerIsNil
+	}
+
+	if err := s.server.Serve(s.listener); err != nil {
+		return fmt.Errorf("grpc server serve: %w", err)
+	}
+
+	return nil
+}
+
+func (s *GRPCWrapper) Shutdown() error {
+	s.server.GracefulStop()
 
 	return nil
 }
